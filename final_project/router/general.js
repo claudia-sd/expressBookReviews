1b15fc02ffcf274f97e39fda516eb9f99fe76d8c
claudia-sd <claudiasentinelli@gmail.com>
2024-10-04 17:31:00 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,13 +28,47 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author; // Get the author from request parameters
+  const matchedBooks = []; // Create an array to store matched books
+
+  // Iterate over each key in the books object
+  const keys = Object.keys(books);
+  for (let i = 0; i < keys.length; i++) {
+      const book = books[keys[i]]; // Get the book using the key
+      if (book.author === author) { // Check if the author matches
+          matchedBooks.push(book); // If so, push it to the matchedBooks array
+      }
+  }
+  // Check if any books were found
+  if (matchedBooks.length > 0) {
+      res.status(200).json(matchedBooks); // Send the matched books as a response
+  } else {
+      res.status(404).json({ message: "No books found for this author" }); // No books found
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title; // Get the title from request parameters
+  const matchedBooks = []; // Create an array to store matched books
+
+  // Iterate over each key in the books object
+  const keys = Object.keys(books);
+  for (let i = 0; i < keys.length; i++) {
+      const book = books[keys[i]]; // Get the book using the key
+      if (book.title === title) { // Check if the author matches
+          matchedBooks.push(book); // If so, push it to the matchedBooks array
+      }
+  }
+  // Check if any books were found
+  if (matchedBooks.length > 0) {
+      res.status(200).json(matchedBooks); // Send the matched books as a response
+  } else {
+      res.status(404).json({ message: "No books found for this title" }); // No books found
+  }
 });
 
 //  Get book review
